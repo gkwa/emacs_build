cbecea5a1d6c1427f51c673a9c7d34555bf3902a
boxstream <taylor.monacelli@streambox.com>
2012-09-08 17:57:34 -0700

    When using stow, don't use generic /usr/local/stow/emacs folder
    Instead of stowing emacs v24.2.50 to here /usr/local/stow/emacs, stow it
    here /usr/local/stow/emacs-24.2.50.
    
    By stowing it in the build specific versioned directory
    /usr/local/stow/emacs-24.2.50, you can then freely install a new emacs
    by installing to /usr/local/stow/emacs-25.0.0 (or whatever) and then
    only upgrading to v25 when ready.
    
    Change-Id: I249ad5db5ae3afb4c05d06e02fbd7c787e444a0f

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ if test ! -z $?; then
    echo we relying on stow here, please 'brew install stow' first
 fi
 
+# brew install stow #http://www.inductiveload.com/posts/basic-use-of-gnu-stowxstow/
 cd ~/play/emacs
 echo "Building @`date`" | tee -a build.log
 make clean 2>/dev/null
@@ -46,10 +47,14 @@ sed "s/$VNUM,/$VERS,/" < $STRINGS > ${STRINGS}.tmp
 mv ${STRINGS}.tmp $STRINGS
 unset EMACSDATA; unset EMACSDOC; echo $EMACSDATA $EMACSDOC;
 make configure
-CFLAGS="-pipe -march=nocona" ./configure --prefix=/usr/local/stow/emacs --build i686-apple-darwin10.0.0 --with-ns
+
+edir=emacs-$VNUM
+CFLAGS="-pipe -march=nocona" ./configure --prefix=/usr/local/stow/$edir --build i686-apple-darwin10.0.0 --with-x
+
 # http://www.emacswiki.org/emacs/BuildingEmacs#toc2
-make bootstrap -j3 && make install prefix=/usr/local/stow/emacs
-stow emacs
+time make bootstrap -j3 && time make install prefix=/usr/local/stow/$edir
+cd /usr/local/stow
+stow $edir
 EOF
 
 sh -x build1.sh
