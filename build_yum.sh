0d8ac6adb83e0e909d25c75e1232026d16c2ae22
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-08-23 22:15:54 -0700

    Install emacs on Amazon EC2 CentOS AMI

--- build_yum.sh
+++ build_yum.sh
@@ -1,10 +1,14 @@
-yum -y install autoconf automake gcc ncurses-devel git
+yum -y install autoconf automake gcc ncurses-devel git stow
 
 mkdir -p /usr/local/src
+mkdir -p /usr/local/stow
 cd /usr/local/src
 
 git clone -b emacs-24 https://github.com/emacs-mirror/emacs.git
 cd emacs
 ./autogen.sh
-./configure --without-makeinfo
+./configure --without-makeinfo --prefix=/usr/local/stow/emacs-24
 make install
+
+cd /usr/local/stow
+stow emacs
