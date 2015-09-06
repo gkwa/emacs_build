54dc9f7ce3299eea946458ab98e1270e293a83d5
Taylor Monacelli <taylormonacelli@gmail.com>
2015-09-05 19:16:38 -0700

    Build emacs on apt-get compatible machine

---
+++ emacs-install-apt.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt-get -y -qq install build-essential libncurses-dev
+
+vermajor=24
+verminor=4
+ver=$vermajor.$verminor
+
+mkdir -p /usr/local/src
+cd /usr/local/src
+curl -O http://ftp.gnu.org/gnu/emacs/emacs-$ver.tar.xz
+tar xf emacs-$ver.tar.xz
+cd emacs-$ver
+mkdir -p /usr/local/stow
+./autogen.sh
+./configure --without-makeinfo --prefix=/usr/local/stow/emacs-$ver
+make install
